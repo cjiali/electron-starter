500352240325b3900d199a010a2627e16ebd5447
cjiali <chjiali@outlook.com>
2021-01-02 21:03:52 +0800

    feat: custom react app

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,10 @@
-module.exports = function override(config, env) {
-  // do stuff with the webpack config...
-  return config;
+const  path = require('path');
+const { override, addWebpackAlias } = require('customize-cra');
+
+module.exports = {
+  webpack: override(
+    addWebpackAlias({
+        '@': path.join(__dirname, "src")
+    }), // Configure path alias
+  ),
 };
